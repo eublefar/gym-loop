e94337fef845db91d6b0fe4c566544df8070c18e
eublefar <evil.unicorn1@gmail.com>
2020-06-28 21:38:26 +0200

    Added commands to parse and execute yaml run configs

--- botbowl_bot/cli.py
+++ botbowl_bot/cli.py
@@ -1,15 +1,84 @@
 """Console script for botbowl_bot."""
 import sys
 import click
+import yaml
+from botbowl_bot.botbowl_bot import (
+    train_agent,
+    eval_agent,
+    get_default_params,
+    validate_module_str,
+)
 
 
-@click.command()
-def main(args=None):
+@click.group()
+def main(run_config):
     """Console script for botbowl_bot."""
-    click.echo("Replace this message by putting your code into "
-               "botbowl_bot.cli.main")
-    click.echo("See click documentation at https://click.palletsprojects.com/")
     return 0
+
+
+@main.command()
+@click.option(
+    "-c",
+    "--run-config",
+    required=True,
+    type=click.File("r"),
+    help="Path to run parameters YAML file",
+)
+def train(run_config):
+    """Run agent training experiment specified in run config"""
+    train_agent(yaml.load(run_config.read()))
+
+
+@main.command()
+@click.option(
+    "-c",
+    "--run-config",
+    required=True,
+    type=click.File("r"),
+    help="Path to run parameters YAML file",
+)
+def eval(run_config):
+    """Evaluate agent specified in run config without updating"""
+    eval_agent(yaml.load(run_config.read()))
+
+
+def validate_module_option(ctx, param, value):
+    if validate_module_str(value):
+        return
+    else:
+        click.echo("Module string must be of format package.module:Class")
+        ctx.exit()
+
+
+@main.command()
+@click.option(
+    "-c",
+    "--run-config",
+    required=True,
+    type=click.File("w"),
+    help="Path to where yaml parameter file will be generated",
+)
+@click.option(
+    "-a",
+    "--agent",
+    default="botbowl_bot.agents.random_agent:RandomAgent",
+    type=click.STRING,
+    callback=validate_module_option,
+    help="Agent string for which spec will be generated",
+)
+@click.option(
+    "-a",
+    "--loop",
+    default="botbowl_bot.loops.default_loop:DefaultLoop",
+    type=click.STRING,
+    callback=validate_module_option,
+    help="Loop string for which spec will be generated",
+)
+def create_default(run_config, agent, loop):
+    """Produce default run config for an agent"""
+    params = get_default_params(agent, loop)
+    yaml_str = yaml.dump(params)
+    run_config.write(yaml_str)
 
 
 if __name__ == "__main__":
